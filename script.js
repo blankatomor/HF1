a85ac6535aeed74f81369c24e26e335db74ee505
blankatomor <xlanatomorx@gmail.com>
2024-10-05 19:41:56 +0200

    telefonszám hozzáadás

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ window.addEventListener('load', function (event) {
             console.log(myJson);
             document.getElementById("name").innerHTML = "Szia, " + myJson.results[0].name.first + " " + myJson.results[0].name.last + " vagyok!";
             document.getElementById("email").innerHTML = "Email: " + myJson.results[0].email;
+            document.getElementById("phone").innerHTML = "Telefonszám: " + myJson.results[0].cell;
         }).catch((error) => {
             console.error(error);
             alert('Ajax lekérés hiba!');
